bd84d1991ec1aa289c57cccb1917a28ffb6bd9bc
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-27 15:53:16 +0200

    Remove the dummy interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,30 +5,24 @@ import { Button } from "@/components/ui/button";
 import InterviewCard from "@/components/InterviewCard";
 
 import { getCurrentUser } from "@/lib/actions/auth.action";
-import { getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/general.action";
-
-import { dummyInterviews } from "@/constants"; //dummyInterviews
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
 
 async function Home() {
   const user = await getCurrentUser();
 
-  let userInterviews = [];
-  let allInterview = [];
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
 
-  // Fetch all interviews (available to everyone)
-  //allInterviews = await getLatestInterviews({ userId: user?.id });
-
-  // Fetch user-specific interviews only if the user is authenticated
-  if (user) {
-    userInterviews = await getInterviewsByUserId(user.id);
-  }
-
-  const hasPastInterviews = userInterviews?.length > 0;
-  const hasUpcomingInterviews = allInterview?.length > 0;
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
 
   return (
     <>
-      {/* Public Section: Always visible */}
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
@@ -50,55 +44,47 @@ async function Home() {
         />
       </section>
 
-      {/* User-Specific Section: Only visible if the user is authenticated */}
-      {user && (
-        <section className="flex flex-col gap-6 mt-8">
-          <h2>Your Interviews</h2>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
 
-          <div className="interviews-section">
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
+        </div>
+      </section>
 
-            {dummyInterviews.map((interview) => (  
-            
-            <InterviewCard {...interview}
-              key={interview.id}
-              userId={user?.id}
-              interviewId={interview.id}
-              role={interview.role}
-              type={interview.type}
-              techstack={interview.techstack}
-              createdAt={interview.createdAt}/>
-          
-            ))}
-            
-
-            {/*   <p>You haven&apos;t taken any interviews yet</p> */}
-           
-          </div>
-        </section>
-      )}
-
-      {/* Public Section: Always visible (Take Interviews) */}
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take Interviews</h2>
 
         <div className="interviews-section">
-
-          {dummyInterviews.map((interview) => (
-          
-          <InterviewCard {...interview}
-            key={interview.id}
-            userId={user?.id}
-            interviewId={interview.id}
-            role={interview.role}
-            type={interview.type}
-            techstack={interview.techstack}
-            createdAt={interview.createdAt}
-            />
-            
-            ))}
-
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
             <p>There are no interviews available</p>
-          
+          )}
         </div>
       </section>
     </>
