833ee8acf17f23096aa52ab3b8fd3f48ecf5cf05
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-28 17:04:02 +0200

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import { Toaster } from "sonner";
-import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
-
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
 
-const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
+const inter = Inter({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Add weights as needed
 });
 
 export const metadata: Metadata = {
-  title: "PrepSmart",
-  description: "An AI Powered Interview Preparation Platform",
-  keywords: ["interview", "ai", "preparation", "questions", "answers"],
+  title:
+    "PrepSmart: AI-Powered Real-Time Interview Platform for Smarter Hiring",
+  description:
+    "PrepSmart.AI is an intelligent voice-driven interview platform that helps companies assess candidates in real time using conversational AI",
 };
 
 export default function RootLayout({
@@ -22,11 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${monaSans.className} antialiased`}>
+      <body className={`${inter.variable} antialiased`}>
         {children}
-
-        <Toaster />
+        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
 }
+
