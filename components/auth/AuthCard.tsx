0c6cdb598925574d6656fec373b9b8af98decdf3
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-30 18:31:10 +0200

    add semi-colon to import statement

--- components/auth/AuthCard.tsx
+++ components/auth/AuthCard.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import {
   Card,
   CardContent,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 interface AuthCardProps {
   children: ReactNode;
