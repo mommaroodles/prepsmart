4b16fcdf6c8d7abac73ebefbb15d69a776d3ba0a
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-30 02:16:32 +0200

    Add Meta Viewport to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner"
+import type { Viewport } from 'next'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,12 +11,19 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title:
-    "PrepSmart: AI-Powered Real-Time Interview Platform for Smarter Hiring",
+    "PrepSmart - AI Powered Real Time Interview Platform for Smarter Hiring",
   description:
     "PrepSmart.AI is an intelligent voice-driven interview platform that helps companies assess candidates in real time using conversational AI",
   keywords:
     "interview platform, interview software, interview tool, interview platform for hiring, interview platform for recruiters, interview platform for companies, interview platform for employers, interview platform for job seekers, interview platform for hiring managers, interview platform for recruiters"
 };
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  colorScheme: 'dark'
+}
 
 export default function RootLayout({
   children,
